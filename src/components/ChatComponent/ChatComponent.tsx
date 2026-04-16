7905d5eeb3cf33e7a9a9bec8531a3a1bbdbc65d9
Alexandr <alexandron2310@gmail.com>
2026-04-16 23:45:03 +0300

    klassniy css bro

--- src/components/ChatComponent/ChatComponent.tsx
+++ src/components/ChatComponent/ChatComponent.tsx
@@ -1,14 +1,25 @@
 import type { Chat } from "../../types/Chat";
 
-export default function ChatComponent(props: {chat: Chat}) {
-    console.log(props.chat.messages)
+export default function ChatComponent(props: { chat: Chat }) {
+    const lastMessage = props.chat.messages[props.chat.messages.length - 1];
+
     return (
-        <div>
-            <div>название чата: {props.chat.title}</div>
-            <div>аватарка: {props.chat.avatar}</div>
-            <div>айди чата: {props.chat.id}</div>
-            <div>последнее сообщение: {props.chat.messages[props.chat.messages.length -1].text}</div>
-            <div>други: {props.chat.users[0].username}</div>
+        <div className="chat-card">
+            <div className="chat-header">
+                <div className="chat-avatar">
+                    <img src={props.chat.avatar} alt="avatar" />
+                </div>
+                <div className="chat-title">{props.chat.title}</div>
+            </div>
+
+            <div className="chat-body">
+                <div className="chat-last-message">
+                    {lastMessage?.text}
+                </div>
+                <div className="chat-user">
+                    {props.chat.users[0]?.username}
+                </div>
+            </div>
         </div>
-    )
-}   
+    );
+}
